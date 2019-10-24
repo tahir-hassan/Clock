b28234c458d0819552e96fd3b817595f54950bce
Hassan, Tahir <th203@leicester.ac.uk>
2019-10-24 19:38:06 +0100

    made the window into more of a dialog using code found at stackoverflow.com: https://stackoverflow.com/a/2910600/288393

--- Clock/ClockForm.cs
+++ Clock/ClockForm.cs
@@ -21,6 +21,31 @@ namespace Clock
             this.Timer.Tick += Timer_Tick;
             this.Timer.Start();
             InitializeComponent();
+            Activated += ClockForm_Activated;
+        }
+
+        private void ClockForm_Activated(object sender, EventArgs e)
+        {
+            var form = Form.ActiveForm;
+            form.TopMost = true;
+
+            // Define the border style of the form to a dialog box.
+            form.FormBorderStyle = FormBorderStyle.FixedDialog;
+
+            // Set the MaximizeBox to false to remove the maximize box.
+            form.MaximizeBox = false;
+
+            // Set the MinimizeBox to false to remove the minimize box.
+            form.MinimizeBox = false;
+
+            // Set the start position of the form to the center of the screen.
+            form.StartPosition = FormStartPosition.CenterScreen;
+
+            // Display the form as a modal dialog box.
+            form.ShowDialog();
+
+
+
         }
 
         private void Timer_Tick(object sender, EventArgs e)
