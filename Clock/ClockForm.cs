8273202e3e09ca545db47d3e8b4b49843d50046a
Hassan, Tahir <th203@leicester.ac.uk>
2019-10-25 14:15:11 +0100

    added a command line argument which specifies the number of seconds forward or backward you want from the system clock.  also + and - buttons on the keyboard increase and decrease the time by one second.

--- Clock/ClockForm.cs
+++ Clock/ClockForm.cs
@@ -13,11 +13,12 @@ namespace Clock
     public partial class ClockForm : Form
     {
         private Timer Timer { get; set; }
+        public int OffsetInSeconds { get; set; } = 0;
 
         public ClockForm()
         {
             this.Timer = new Timer();
-            this.Timer.Interval = 500;
+            this.Timer.Interval = 100;
             this.Timer.Tick += Timer_Tick;
             this.Timer.Start();
             InitializeComponent();
@@ -35,13 +36,29 @@ namespace Clock
             // Set the MinimizeBox to false to remove the minimize box.
             form.MinimizeBox = false;
 
+            form.KeyUp += Form_KeyUp;
+
             // Set the start position of the form to the center of the screen.
             form.StartPosition = FormStartPosition.CenterScreen;
+
+            Activated -= ClockForm_Activated;
+        }
+
+        private void Form_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == (Keys.Shift | Keys.OemMinus))
+            {
+                this.OffsetInSeconds -= 1;
+            }
+            else if (e.KeyData == (Keys.Shift | Keys.Oemplus))
+            {
+                this.OffsetInSeconds += 1;
+            }
         }
 
         private void Timer_Tick(object sender, EventArgs e)
         {
-            var now = DateTime.Now;
+            var now = DateTime.Now.AddSeconds(this.OffsetInSeconds);
             this.TimeLabel.Text = now.ToString("HH:mm");
             this.SecondsLabel.Text = now.ToString(":ss");
         }
--- Clock/Program.cs
+++ Clock/Program.cs
@@ -6,17 +6,21 @@ using System.Windows.Forms;
 
 namespace Clock
 {
-    static class Program
+    internal static class Program
     {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        private static void Main(string[] args)
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ClockForm());
+
+            int offset = 0;
+            int.TryParse(args.FirstOrDefault(), out offset);
+
+            Application.Run(new ClockForm() { OffsetInSeconds = offset });
         }
     }
 }
