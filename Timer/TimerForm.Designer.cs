52a599c1d4cee76fb4ab3c685309c3184c82cdf0
Hassan, Tahir <th203@leicester.ac.uk>
2021-03-25 21:22:31 +0000

    Timer is a separate timer GUI app

---
+++ Timer/TimerForm.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Timer
+{
+    partial class TimerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TimerForm));
+            this.TimeLabel = new System.Windows.Forms.Label();
+            this.SecondsLabel = new System.Windows.Forms.Label();
+            this.PausePlayButton = new System.Windows.Forms.Button();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.AddOneButton = new System.Windows.Forms.Button();
+            this.AddFiveButton = new System.Windows.Forms.Button();
+            this.AddThirtyButton = new System.Windows.Forms.Button();
+            this.AddFifteenButton = new System.Windows.Forms.Button();
+            this.LogTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // TimeLabel
+            // 
+            this.TimeLabel.AutoSize = true;
+            this.TimeLabel.Font = new System.Drawing.Font("Consolas", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TimeLabel.Location = new System.Drawing.Point(4, 1);
+            this.TimeLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.TimeLabel.Name = "TimeLabel";
+            this.TimeLabel.Size = new System.Drawing.Size(195, 70);
+            this.TimeLabel.TabIndex = 0;
+            this.TimeLabel.Text = "00:00";
+            // 
+            // SecondsLabel
+            // 
+            this.SecondsLabel.AutoSize = true;
+            this.SecondsLabel.BackColor = System.Drawing.Color.Transparent;
+            this.SecondsLabel.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SecondsLabel.Location = new System.Drawing.Point(193, 36);
+            this.SecondsLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.SecondsLabel.Name = "SecondsLabel";
+            this.SecondsLabel.Size = new System.Drawing.Size(51, 28);
+            this.SecondsLabel.TabIndex = 1;
+            this.SecondsLabel.Text = ":00";
+            // 
+            // PausePlayButton
+            // 
+            this.PausePlayButton.Font = new System.Drawing.Font("Webdings", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PausePlayButton.Location = new System.Drawing.Point(16, 74);
+            this.PausePlayButton.Name = "PausePlayButton";
+            this.PausePlayButton.Size = new System.Drawing.Size(75, 60);
+            this.PausePlayButton.TabIndex = 2;
+            this.PausePlayButton.Text = "|| >";
+            this.PausePlayButton.UseVisualStyleBackColor = true;
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Font = new System.Drawing.Font("Webdings", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ClearButton.Location = new System.Drawing.Point(166, 74);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(75, 60);
+            this.ClearButton.TabIndex = 3;
+            this.ClearButton.Text = "X";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            // 
+            // AddOneButton
+            // 
+            this.AddOneButton.Font = new System.Drawing.Font("Webdings", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AddOneButton.Location = new System.Drawing.Point(16, 160);
+            this.AddOneButton.Name = "AddOneButton";
+            this.AddOneButton.Size = new System.Drawing.Size(75, 60);
+            this.AddOneButton.TabIndex = 4;
+            this.AddOneButton.Text = "+1";
+            this.AddOneButton.UseVisualStyleBackColor = true;
+            // 
+            // AddFiveButton
+            // 
+            this.AddFiveButton.Font = new System.Drawing.Font("Webdings", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AddFiveButton.Location = new System.Drawing.Point(166, 160);
+            this.AddFiveButton.Name = "AddFiveButton";
+            this.AddFiveButton.Size = new System.Drawing.Size(75, 60);
+            this.AddFiveButton.TabIndex = 5;
+            this.AddFiveButton.Text = "+5";
+            this.AddFiveButton.UseVisualStyleBackColor = true;
+            // 
+            // AddThirtyButton
+            // 
+            this.AddThirtyButton.Font = new System.Drawing.Font("Webdings", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AddThirtyButton.Location = new System.Drawing.Point(166, 249);
+            this.AddThirtyButton.Name = "AddThirtyButton";
+            this.AddThirtyButton.Size = new System.Drawing.Size(75, 60);
+            this.AddThirtyButton.TabIndex = 7;
+            this.AddThirtyButton.Text = "+30";
+            this.AddThirtyButton.UseVisualStyleBackColor = true;
+            // 
+            // AddFifteenButton
+            // 
+            this.AddFifteenButton.Font = new System.Drawing.Font("Webdings", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AddFifteenButton.Location = new System.Drawing.Point(16, 249);
+            this.AddFifteenButton.Name = "AddFifteenButton";
+            this.AddFifteenButton.Size = new System.Drawing.Size(75, 60);
+            this.AddFifteenButton.TabIndex = 6;
+            this.AddFifteenButton.Text = "+15";
+            this.AddFifteenButton.UseVisualStyleBackColor = true;
+            // 
+            // LogTextBox
+            // 
+            this.LogTextBox.Location = new System.Drawing.Point(269, 26);
+            this.LogTextBox.Multiline = true;
+            this.LogTextBox.Name = "LogTextBox";
+            this.LogTextBox.Size = new System.Drawing.Size(320, 283);
+            this.LogTextBox.TabIndex = 8;
+            // 
+            // TimerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(612, 330);
+            this.Controls.Add(this.LogTextBox);
+            this.Controls.Add(this.AddThirtyButton);
+            this.Controls.Add(this.AddFifteenButton);
+            this.Controls.Add(this.AddFiveButton);
+            this.Controls.Add(this.AddOneButton);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.PausePlayButton);
+            this.Controls.Add(this.SecondsLabel);
+            this.Controls.Add(this.TimeLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "TimerForm";
+            this.Text = "Timer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label TimeLabel;
+        private System.Windows.Forms.Label SecondsLabel;
+        private System.Windows.Forms.Button PausePlayButton;
+        private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.Button AddOneButton;
+        private System.Windows.Forms.Button AddFiveButton;
+        private System.Windows.Forms.Button AddThirtyButton;
+        private System.Windows.Forms.Button AddFifteenButton;
+        private System.Windows.Forms.TextBox LogTextBox;
+    }
+}
+
---
+++ Timer/TimerForm.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Timer
+{
+    public partial class TimerForm : Form
+    {
+        private bool AlarmStarted = false;
+        private SoundPlayer AlarmPlayer = new SoundPlayer();
+
+        private TimeSpan TimerInitialValue { get; set; }
+        private DateTime TimerStartDate { get; set; }
+        private bool IsPaused { get; set; }
+
+        private System.Windows.Forms.Timer Timer { get; set; }
+
+        private static string MechanicalBellSoundLocation()
+        {
+            var loc = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            var dir = System.IO.Path.GetDirectoryName(loc);
+            return System.IO.Path.Combine(dir, "MechanicalBell.wav");
+        }
+        
+        public TimerForm()
+        {
+            this.Timer = new System.Windows.Forms.Timer();
+            this.Timer.Interval = 100;
+            this.Timer.Tick += Timer_Tick;
+            this.Timer.Start();
+            InitializeComponent();
+            this.AlarmPlayer.SoundLocation = MechanicalBellSoundLocation(); // find out the location of the .exe to get the file path.
+            Activated += TimerForm_Activated;
+
+            this.LogTextBox.KeyDown += TextBox_KeyDown_CommonKeyCommands;
+
+            this.IsPaused = true;
+            this.TimerInitialValue = new TimeSpan();
+            DisplayTime(new TimeSpan());
+
+            this.PausePlayButton.Click += Pause_Play_Click;
+            this.ClearButton.Click += ClearButton_Click;
+            this.AddOneButton.Click += AddOneButton_Click;
+            this.AddFiveButton.Click += AddFiveButton_Click;
+            this.AddFifteenButton.Click += AddFifteenButton_Click;
+            this.AddThirtyButton.Click += AddThirtyButton_Click;
+        }
+
+        private static DateTime Now() => DateTime.UtcNow;
+
+        private void AddTime(int minutes)
+        {
+            this.StopAlarm();
+
+            if (minutes == 1 && Debugger.IsAttached)
+            {
+                this.TimerInitialValue += new TimeSpan(0, 0, 0, 5, 0);
+            }
+            else
+            {
+                this.TimerInitialValue += new TimeSpan(0, 0, minutes, 0, 0);
+            }
+        }
+
+        public static void TextBox_KeyDown_CommonKeyCommands(object sender, KeyEventArgs e)
+        {
+            var textbox = (sender as TextBox);
+            if (textbox == null)
+            {
+                return;
+            }
+            // Add support for CTRL+A  
+            if (e.Control && e.KeyCode == Keys.A)
+            {
+                textbox.SelectAll();
+            }
+            // Add support for CTRL+Backspace  
+            if (e.Control && e.KeyCode == Keys.Back)
+            {
+                e.SuppressKeyPress = true;
+                if (textbox.SelectionStart > 0)
+                {
+                    /*  
+                     * Piggyback off of the supported "CTRL + Left Cursor" feature.  
+                     * Does not need to send {CTRL}, because the user is currently holding {CTRL}.  
+                     * Uses {DEL} rather than {BKSP} in order to avoid creating an infinite loop.  
+                     * NOTE: {DEL} has the side effect of deleting text to the right if the cursor is  
+                     *       already as far left as it can go, since no text will be selected by {LEFT}.  
+                     *       The .SelectionStart > 0 condition prevents this side effect.  
+                     */
+                    SendKeys.Send("+{LEFT}{DEL}");
+                }
+            }
+        }
+
+        private void LogMessage(string message)
+        {
+            var logLine = $"{DateTime.Now.ToString("HH:mm ss")}|{message}";
+            
+            if (this.LogTextBox.Text == "")
+            {
+                this.LogTextBox.Text = logLine;
+            }
+            else
+            {
+                var existingLines = this.LogTextBox.Text.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+                if (existingLines.Last() == string.Empty)
+                {
+                    var newValue = string.Join(Environment.NewLine, existingLines.Take(existingLines.Length - 1).Concat(new[] { logLine }));
+                    this.LogTextBox.Text = newValue;
+                }
+                else
+                {
+                    var newValue = string.Join(Environment.NewLine, existingLines.Concat(new[] { logLine }));
+                    this.LogTextBox.Text = newValue;
+                }
+            }
+        }
+
+        private void Pause(bool logMessage = true)
+        {
+            this.StopAlarm();
+            if (!this.IsPaused)
+            {
+                this.IsPaused = true;
+                var elapsed = Now() - TimerStartDate;
+
+                // this should be in a method?
+                this.TimerInitialValue -= elapsed;
+                if (this.TimerInitialValue < new TimeSpan())
+                {
+                    this.TimerInitialValue = new TimeSpan();
+                }
+
+                if (logMessage)
+                {
+                    LogMessage($"Paused with time left {GetTimeString(this.TimerInitialValue)}");
+                }
+            }
+        }
+
+        
+
+        private void Play()
+        {
+            this.StopAlarm();
+            if (this.IsPaused && this.TimerInitialValue > new TimeSpan())
+            {
+                LogMessage($"Play with time left {GetTimeString(this.TimerInitialValue)}");
+                this.IsPaused = false;
+                this.TimerStartDate = Now();
+            }            
+        }
+
+        private void Clear()
+        {
+            this.StopAlarm();
+            Pause(logMessage: false);
+            LogMessage($"Cleared with time left {GetTimeString(this.TimerInitialValue)}");
+            this.TimerInitialValue = new TimeSpan();
+        }
+
+        private void PausePlay()
+        {
+            if (IsPaused)
+            {
+                Play();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+
+
+        private void AddThirtyButton_Click(object sender, EventArgs e)
+        {
+            AddTime(30);
+        }
+
+        private void AddFifteenButton_Click(object sender, EventArgs e)
+        {
+            AddTime(15);
+        }
+
+        private void AddFiveButton_Click(object sender, EventArgs e)
+        {
+            AddTime(5);
+        }
+
+        private void AddOneButton_Click(object sender, EventArgs e)
+        {
+            AddTime(1);
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            //if (this.IsPaused && this.TimerInitialValue == new TimeSpan())
+            //{
+            //    PlaySound();
+            //}
+            Clear();
+        }
+
+        private void Pause_Play_Click(object sender, EventArgs e)
+        {
+            PausePlay();
+        }
+
+        private void TimerForm_Activated(object sender, EventArgs e)
+        {
+            var form = Form.ActiveForm;
+            form.TopMost = false;
+
+            // Set the MaximizeBox to false to remove the maximize box.
+            form.MaximizeBox = false;
+
+            // Set the MinimizeBox to false to remove the minimize box.
+            form.MinimizeBox = true;
+
+            form.KeyUp += Form_KeyUp;
+
+            // Set the start position of the form to the center of the screen.
+            form.StartPosition = FormStartPosition.CenterScreen;
+
+            Activated -= TimerForm_Activated;
+        }
+
+        private void Form_KeyUp(object sender, KeyEventArgs e)
+        {
+            // this technique can be used to handle keypresses
+            if (e.KeyData == (Keys.Shift | Keys.OemMinus))
+            {
+            }
+            else if (e.KeyData == (Keys.Shift | Keys.Oemplus))
+            {
+            }
+        }
+
+        private string GetTimeString(TimeSpan time)
+        {
+            var hoursMinutes = time.ToString(@"hh\:mm");
+            var seconds = time.ToString(@"\:ss");
+            return hoursMinutes + " " + seconds;
+        }
+
+        private void DisplayTime(TimeSpan time)
+        {
+            this.TimeLabel.Text = time.ToString(@"hh\:mm");
+            this.SecondsLabel.Text = time.ToString(@"\:ss");
+            this.Text = this.TimeLabel.Text + " " + this.SecondsLabel.Text;
+        }
+
+        private void StopAlarm()
+        {
+            if (AlarmStarted)
+            {
+                this.AlarmPlayer.Stop();
+                LogMessage("Alarm stopped");
+                AlarmStarted = false;
+            }           
+        }
+
+        private void StartAlarm()
+        {
+            if (!AlarmStarted)
+            {
+                this.AlarmPlayer.PlayLooping();
+                LogMessage("Alarm started");
+                AlarmStarted = true;
+            }            
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (!this.IsPaused)
+            {
+                var elapsed = Now() - TimerStartDate;
+                var remainingTime = TimerInitialValue - elapsed;
+                if (remainingTime < new TimeSpan())
+                {
+                    Clear();
+                    StartAlarm();
+                    DisplayTime(new TimeSpan());
+                }
+                else
+                {
+                    DisplayTime(remainingTime);
+                }
+            }
+
+            if (this.IsPaused)
+            {
+                DisplayTime(this.TimerInitialValue);
+            }
+        }
+    }
+}
